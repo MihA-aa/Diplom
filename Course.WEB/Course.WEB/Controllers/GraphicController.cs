bd1711ad18ae890ee7e83723fb3477f2d9752234
m.ermoalev <m.ermolaev@itransition.com>
2018-09-17 20:54:21 +0300

    Added graphic part

---
+++ Course.WEB/Course.WEB/Controllers/GraphicController.cs
@@ -0,0 +1,18 @@
+﻿using Course.WEB.Models.Repositories;
+using System.Web.Mvc;
+using System.Linq;
+using Course.WEB.Models.MyViewModel;
+
+namespace Course.WEB.Controllers
+{
+    public class GraphicController : Controller
+    {
+        readonly EFUnitOfWork db = new EFUnitOfWork();
+
+        [HttpGet]
+        public void Get()
+        {
+            Response.Redirect("http://localhost:9847/Scripts/Graphics/index.html");
+        }
+    }
+}
